2afaae0963af3c8bb9c47567b56e6d38c7dfdece
ukoptimization <87356700+ukoptimization@users.noreply.github.com>
2021-07-13 09:42:04 +0300

    Add files via upload

---
+++ bs5/wordup.js
@@ -0,0 +1,158 @@
+/* global CKEDITOR, TurndownService */
+var turndownService = new TurndownService({
+  bulletListMarker: "-",
+  headingStyle: "atx"
+  
+});
+// Configure CKEditor window
+CKEDITOR.replace("wordup", {
+  dataIndentationChars: "  ",
+  format_tags: "p;h1;h2;h3;h4;h5",
+  height: 325,
+  removeButtons:
+    "Underline,Strike,Subscript,Superscript,Anchor,Styles,Specialchar",
+  toolbarGroups: [
+    { groups: ["basicstyles"], name: "basicstyles" },
+    { groups: ["links"], name: "links" },
+    { groups: ["list"], name: "paragraph" },
+    { groups: ["list"], name: "insert" },
+    { groups: ["mode"], name: "document" },
+    { groups: ["styles"], name: "styles" }
+  ]
+});
+
+function scrubber(string) {
+  brandsArray = [
+    "Lion", 
+    "Gerber", 
+    "Test", 
+    "full"];
+   
+  let scrubbed = string;
+  scrubbed = scrubbed
+
+    .replace(/<p><strong>&nbsp;<\/strong><\/p>/g, "")
+    .replace(/<strong>&nbsp;<\/strong>/g, "")
+    .replace(/<p>&nbsp;<\/p>/g, "")
+    .replace(/<h1>&nbsp;<\/h1>/g, "")
+    .replace(/<h2>&nbsp;<\/h2>/g, "")
+    .replace(/<h3>&nbsp;<\/h3>/g, "")
+    .replace(/<h4>&nbsp;<\/h4>/g, "")
+    .replace(/<h5>&nbsp;<\/h5>/g, "")
+    .replace(/<(h[^>])+><strong>(.*)<\/strong><\/h[^>]+>/g, "<$1>$2</$1>")
+    .replace(/<(h[^>])+><em>(.*)<\/em><\/h[^>]+>/g, "<$1>$2</$1>")
+    .replace(/<(table|td|tr|th)\s+width="(\d+?)">/g, "<$1>")
+    .replace(/<\/strong><strong>/g, "")
+    .replace(/&ndash;ndash;/g, "&mdash;")
+    .replace(/\n\n\n/g, "\n\n")
+    .replace(/\n\n/g, "\n")
+    .replace(
+      /&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/g,
+      "&nbsp;"
+    )
+    .replace(
+      /&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/g,
+      "&nbsp;"
+    )
+    .replace(/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/g, "&nbsp;")
+    .replace(/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/g, "&nbsp;")
+    .replace(/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/g, "&nbsp;")
+    .replace(/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/g, "&nbsp;")
+    .replace(/&nbsp;&nbsp;&nbsp;&nbsp;/g, "&nbsp;")
+    .replace(/&nbsp;&nbsp;&nbsp;/g, "&nbsp;")
+    .replace(/&nbsp;&nbsp;/g, "&nbsp;")
+    .replace(/&nbsp;/g, " ");
+  
+    let resultBrands = scrubbed;
+    for (let i in brandsArray) {
+    resultBrands = resultBrands.replace(new RegExp('('+brandsArray[i]+')(?!<\\/b>)', "i"), "<em>$1</em>");
+  }
+  scrubbed = resultBrands
+  return resultBrands;
+
+//взяли ми такі все поміняли, і після цього поміняли бренди з масива
+}
+
+// function brands(string) {
+//   brandsArray = ["Lion", "Gerber", "Test", "full"];
+//   let resultBrands = string;
+//   for (let i in brandsArray) {
+//   resultBrands = resultBrands.replace(new RegExp('('+brandsArray[i]+')(?!<\\/b>)', "i"), "<em>$1</em>");
+//   }
+//   }
+  
+function regexEscape(string) {
+
+  return string.replace(/[-/\\^$*+?.()|[\]{}]/g, "\\$&");
+
+
+}
+
+function reg(string) {
+  var escaped = string;
+  var flags = "g";
+  var escapedNum = Number;
+  escaped = regexEscape(escaped);
+  escaped = "https?://" + escaped;
+  ++escapedNum;
+  console.log(escapedNum);
+  return new RegExp(escaped, flags);
+ 
+}
+ 
+function addDomainFilter(string, domain) {
+  var filtered = string;
+  var domainregex = reg(domain);
+
+  filtered = filtered.replace(domainregex, "");
+
+  return filtered;
+}
+
+function addTargetBlank(string) {
+  var plusTarget = string;
+
+  plusTarget = plusTarget.replace(
+    /<(a\s+(?:[^>]*?\s+)?href="https?([^"]*)")/g,
+    '<$1 target="_blank" class="externalLink"'
+  );
+
+  return plusTarget;
+}
+
+function clearBoth() {
+  CKEDITOR.instances.wordup.setData("");
+  document.getElementById("output").value = "";
+}
+
+function wordup() {
+  var pasteData = CKEDITOR.instances.wordup.getData();
+
+  pasteData = scrubber(pasteData);
+  
+  
+  if (
+    document.getElementById("domainfilter").checked === true &&
+    document.getElementById("domainname").value
+  ) {
+    pasteData = addDomainFilter(
+      pasteData,
+      document.getElementById("domainname").value
+    );
+  }
+
+  if (document.getElementById("targetblank").checked === true) {
+    pasteData = addTargetBlank(pasteData);
+  }
+
+  if (document.getElementById("markdown").checked === true) {
+    pasteData = turndownService.turndown(pasteData);
+  }
+
+  document.getElementById("output").value = pasteData;
+}
+document.getElementById("clear").addEventListener("click", clearBoth);
+document.getElementById("convert").addEventListener("click", wordup);
+document.getElementById("targetblank").addEventListener("click", wordup);
+document.getElementById("markdown").addEventListener("click", wordup);
+document.getElementById("domainfilter").addEventListener("click", wordup);
